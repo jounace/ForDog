d86322d63e2bd2b55c99a52e894efc5ac6cb5506
黄子豪 <huangzihao@huangzihaodeMacBook-Pro.local>
2024-03-03 01:26:42 +0800

    update

--- fordog/src/main/java/un/uw/walkthedog/operation/dao/OperationServiceDao.java
+++ fordog/src/main/java/un/uw/walkthedog/operation/dao/OperationServiceDao.java
@@ -2,7 +2,6 @@ package un.uw.walkthedog.operation.dao;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.data.relational.core.sql.SQL;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
 
